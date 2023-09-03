58e2888aa2cd84214980b400679cf749aebf580c
Shawn Baltar <shawnnbaltar@gmail.com>
2023-09-03 16:14:14 -0700

    Add theme toggle

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,34 @@
+import { useEffect } from 'react';
+import { FaMoon } from 'react-icons/fa';
+import { ImSun } from 'react-icons/im';
+import { useGlobalContext } from './context';
+
 const Navbar = () => {
+  const {theme, setTheme} = useGlobalContext()
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <h2>where to watch</h2>
+        </div>
+        <div className="btn-container">
+          <button onClick={toggleTheme}>
+            {theme === 'light-theme' ? <ImSun/> : <FaMoon/>}
+          </button>
         </div>
       </div>
     </nav>
--- src/context.jsx
+++ src/context.jsx
@@ -4,12 +4,21 @@ import useFetch from "./useFetch";
 
 const AppContext = React.createContext();
 
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme' )) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+}
+
 const AppProvider = ({ children }) => {
   const [query, setQuery] = useState('');
+  const [theme, setTheme] = useState(getStorageTheme());
   const { isLoading, data: movies } = useFetch(`autocomplete-search`, `&search_value=${query}&search_type=2`)
 
   return (
-    <AppContext.Provider value={{isLoading, query, setQuery, movies}}>
+    <AppContext.Provider value={{isLoading, query, setQuery, movies, theme, setTheme}}>
       {children}
     </AppContext.Provider>
   );
