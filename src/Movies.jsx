911b65b15fbd2318e86856cfb7884be36c2e4209
Shawn Baltar <shawnnbaltar@gmail.com>
2023-09-02 21:39:37 -0700

    Add placeholder image for movies without poster

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,6 +1,8 @@
 import { useGlobalContext } from "./context";
 import { Link } from "react-router-dom";
 
+const url = 'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+
 const Movies = () => {
   const { isLoading, movies } = useGlobalContext()
 
@@ -17,7 +19,7 @@ const Movies = () => {
         return (
           <Link className="movie" key={id} to={`/movies/${id}`}>
             <article>
-              <img src={img} alt={title} />
+              <img src={img === null ? url : img} alt={title} />
               <div className="movie-info">
                 <h4>{title}</h4>
                 <p>{year} </p>
